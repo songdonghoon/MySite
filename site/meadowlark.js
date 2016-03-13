8c23e6d21ad4548799de7dd0c36dfe119da86554
unknown <>
2016-03-13 15:05:48 +0900

    node test

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,25 +2,31 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Hello World');
+	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Hello World');
+	res.render('about');
 });
 
 // custom 404 페이지
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
+app.use(function(err, req, res, next){
+	res.status(500);
+	res.render('500');
+});
 
 
 app.listen(app.get('port'), function(){
